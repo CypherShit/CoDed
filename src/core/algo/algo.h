ef4105a4b9bc942e651e5559838f4a32f0f690f4
Afonkin Pavel <pvafonkin@edu.hse.ru>
2025-02-05 20:20:41 +0000

    fix core arhitecture

--- src/core/algo/algo.h
+++ src/core/algo/algo.h
@@ -2,22 +2,20 @@
 
 #include <core/decoder/decoder.h>
 #include <core/encoder/encoder.h>
-#include <core/salt/salt.h>
 
 #include <string>
 #include <string_view>
 
 namespace core {
 
-template <typename SaltImpl>
-class Algo : public Encoder<SaltImpl>, public Decoder<SaltImpl> {
+class Algo : public Encoder, public Decoder {
 public:
-    const Encoder<SaltImpl>& GetEncoder() const;
-    const Decoder<SaltImpl>& GetDecoder() const;
+    const Encoder& GetEncoder() const;
+    const Decoder& GetDecoder() const;
 
 protected:
-    virtual std::string Decode(std::string_view view, const Salt<SaltImpl>& salt) const = 0;
-    virtual std::string Encode(std::string_view view, const Salt<SaltImpl>& salt) const = 0;
+    virtual std::string Decode(std::string_view view) const = 0;
+    virtual std::string Encode(std::string_view view) const = 0;
 };
 
 } // namespace core
