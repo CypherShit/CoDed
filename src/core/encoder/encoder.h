c64c20719183e751c38e79890e4fe54f1b0d8a4f
Afonkin Pavel <pvafonkin@edu.hse.ru>
2025-02-05 20:20:41 +0000

    fix core arhitecture

--- src/core/encoder/encoder.h
+++ src/core/encoder/encoder.h
@@ -1,16 +1,13 @@
 #pragma once
 
-#include <core/salt/salt.h>
-
 #include <string>
 #include <string_view>
 
 namespace core {
 
-template <typename SaltImpl>
 class Encoder {
 public:
-    virtual std::string Encode(std::string_view view, const Salt<SaltImpl>& salt) const = 0;
+    virtual std::string Encode(std::string_view view) const = 0;
 };
 
 } // namespace core
--- tests/unit/test_caesar_cipher.cpp
+++ tests/unit/test_caesar_cipher.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <core/decoder/decoder.h>
+#include <core/encoder/encoder.h>
 #include <impl/caesar/caesar_algo.h>
 
 namespace {
@@ -16,8 +18,26 @@ int Factorial(int n) {
 
 } // namespace
 
+using namespace coded::impl::caesar;
+
 TEST(TestFactorial, HandlesZeroInput) {
     ASSERT_EQ(1, Factorial(0));
+}
+
+TEST(TestCeasar, HandlesZeroInput) {
+    std::string testString = "abacaba";
+    CaesarCipherAlgo algo(1);
+
+    const core::Decoder& decoder = algo.GetDecoder();
+    const core::Encoder& encoder = algo.GetEncoder();
+
+    const std::string encodedString = encoder.Encode(testString);
+
+    ASSERT_EQ(encodedString, "bcbdbcb");
+
+    const std::string decodedString = decoder.Decode(encodedString);
+
+    ASSERT_EQ(decodedString, testString);
 }
 
 int main(int argc, char**argv) {
