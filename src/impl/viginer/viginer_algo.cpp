2d30731c204aa8d5aa100c7a2f31ec7192eeb216
Afonkin Pavel <pvafonkin@edu.hse.ru>
2025-02-12 19:21:47 +0000

    add vigner

---
+++ src/impl/viginer/viginer_algo.cpp
@@ -0,0 +1,93 @@
+#include "viginer_algo.h"
+
+#include <algorithm>
+#include <cstddef>
+#include <iterator>
+#include <random>
+#include <string>
+#include <string_view>
+
+namespace coded::impl::viginer {
+
+ViginerAlgo::ViginerAlgo() : key_(GenerateRandomKey()){
+}
+
+ViginerAlgo::ViginerAlgo(std::string&& key) : key_(key) {
+}
+
+
+ViginerAlgo::ViginerAlgo(const std::string& key) : key_(key) {
+}
+
+void ViginerAlgo::SetKey(const std::string& key) {
+    key_ = key;
+}
+
+void ViginerAlgo::SetKey(std::string&& key) {
+    std::swap(key_, key);
+}
+
+std::string ViginerAlgo::Encode(std::string_view decodedString) const {
+    std::string result;
+    result.reserve(decodedString.length());
+    for (std::size_t index = 0, keyIndex = 0; index < decodedString.length(); index++, keyIndex++) {
+        if (keyIndex >= key_.length()) {
+            keyIndex -= key_.length();
+        }
+
+        result.push_back(ShiftValue(decodedString[index], key_[keyIndex]));
+    }
+
+    return result;
+}
+
+std::string ViginerAlgo::Decode(std::string_view encodedString) const {
+    std::string result;
+    result.reserve(encodedString.length());
+    for (std::size_t index = 0, keyIndex = 0; index < encodedString.length(); index++, keyIndex++) {
+        if (keyIndex >= key_.length()) {
+            keyIndex -= key_.length();
+        }
+
+        result.push_back(ShiftValue(encodedString[index], key_[keyIndex], true));
+    }
+
+    return result;
+}
+
+char ViginerAlgo::ShiftValue(char symbol, char shiftValue, bool isDecode) {
+    std::int16_t intSymbol = static_cast<std::int16_t>(symbol);
+    std::int16_t intShift = static_cast<std::int16_t>(shiftValue);
+    if (isDecode) {
+        intShift = -intShift;
+    }
+
+    std::int16_t result = intSymbol + intShift;
+
+    if (result > ALPH_MAX_VALUE) {
+        result = ALPH_MIN_VALUE + result - ALPH_MAX_VALUE;
+    }
+
+    if (result < ALPH_MIN_VALUE) {
+        result = ALPH_MAX_VALUE - (ALPH_MIN_VALUE - result);
+    }
+
+    return static_cast<char>(result);
+}
+
+std::string ViginerAlgo::GenerateRandomKey() {
+    static std::uniform_int_distribution<char> DISTRIBUTION(CHAR_MIN, CHAR_MAX);
+
+    std::default_random_engine generator;
+    std::string result;
+    result.reserve(DEFAULT_KEY_LENGTH);
+
+    std::generate_n(std::back_inserter(result), DEFAULT_KEY_LENGTH, [&](){
+        return DISTRIBUTION(generator);
+    });
+
+    return result;
+}
+
+} // namespace coded::impl::viginer
+
---
+++ tests/unit/test_viginer_algo.cpp
@@ -0,0 +1,80 @@
+#include <climits>
+#include <gtest/gtest.h>
+
+#include <core/decoder/decoder.h>
+#include <core/encoder/encoder.h>
+#include <impl/viginer/viginer_algo.h>
+
+
+using namespace coded::impl::viginer;
+
+TEST(DefaultTestViginer, HandlesZeroInput) {
+    const std::string testString = "abacaba";
+    const ViginerAlgo algo;
+
+    const core::Decoder& decoder = algo.GetDecoder();
+    const core::Encoder& encoder = algo.GetEncoder();
+
+    const std::string encodedString = encoder.Encode(testString);
+    const std::string decodedString = decoder.Decode(encodedString);
+
+    ASSERT_EQ(decodedString, testString);
+}
+
+TEST(TestCustomKey, HandlesZeroInput) {
+    const std::string testString = "abacaba";
+    std::string customKey;
+    customKey.push_back(static_cast<char>(0));
+
+    const ViginerAlgo algo(customKey);
+
+    const core::Decoder& decoder = algo.GetDecoder();
+    const core::Encoder& encoder = algo.GetEncoder();
+
+    const std::string encodedString = encoder.Encode(testString);
+
+    ASSERT_EQ(encodedString, testString);
+
+    const std::string decodedString = decoder.Decode(encodedString);
+
+    ASSERT_EQ(decodedString, testString);
+}
+
+TEST(TestChangeKey, HandlesZeroInput) {
+    const std::string testString = "testString";
+
+    ViginerAlgo algo("testKey");
+
+    const core::Decoder& decoder = algo.GetDecoder();
+    const core::Encoder& encoder = algo.GetEncoder();
+
+    const std::string encodedString = encoder.Encode(testString);
+
+    algo.SetKey("new");
+
+    const std::string decodedString = decoder.Decode(encodedString);
+
+    ASSERT_NE(decodedString, testString);
+}
+
+TEST(TestViginerOverflow, HandlesZeroInput) {
+    const std::string testString = "testOverflow";
+
+    std::string maxKey;
+    maxKey.push_back(CHAR_MAX);
+
+    const ViginerAlgo algo(maxKey);
+
+    const core::Decoder& decoder = algo.GetDecoder();
+    const core::Encoder& encoder = algo.GetEncoder();
+
+    const std::string encodedString = encoder.Encode(testString);
+    const std::string decodedString = decoder.Decode(encodedString);
+
+    ASSERT_EQ(decodedString, testString);
+}
+
+int main(int argc, char**argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
